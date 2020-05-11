2eecd1eaf914361198ba995008f5eb5043d06cc0
EC2 Default User <ec2-user@ip-172-31-95-144.ec2.internal>
2020-05-11 15:00:50 +0000

    added virtual network

--- 4-vnet.tf
+++ 4-vnet.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "vnet1" {
+resource "azurerm_virtual_network" "vnet1" {
   name     = "${var.vnet_name}"
   location = "${var.location}"
   resource_group_name ="${azurerm_resource_group.example.name}"
