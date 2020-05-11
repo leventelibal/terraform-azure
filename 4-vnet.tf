ea14c430f1b63d60443d3d9717a0a05c982117b3
EC2 Default User <ec2-user@ip-172-31-95-144.ec2.internal>
2020-05-11 14:55:09 +0000

    added resourse group

--- 4-vnet.tf
+++ 4-vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "vnet1" {
   name     = "${var.vnet_name}"
   location = "${var.location}"
-  resource_group_name ="${var.resource_group_name.example.name}"
+  resource_group_name ="${azurerm_resource_group.example.name}"
   address_space = ["${var.address_space}"]
 }
 #first you have to create resource group you need to do Terraform init and then Terrafrom apply to create 
