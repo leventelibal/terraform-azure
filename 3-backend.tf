28f4344d39931ccdf14a7900db33f3f8f3d492cf
EC2 Default User <ec2-user@ip-172-31-95-144.ec2.internal>
2020-05-11 14:19:45 +0000

    added backend and vnet

---
+++ 3-backend.tf
@@ -0,0 +1,8 @@
+// terraform {
+//   backend "azurerm" {
+//     resource_group_name  = "dev"
+//     storage_account_name = "devenv"
+//     container_name       = "devcontainer"
+//     key                  = "prod.terraform.tfstate"
+//   }
+// }
