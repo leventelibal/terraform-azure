c9be8b8c614b0c1d526aea7656f2f31e24aaa2a7
EC2 Default User <ec2-user@ip-172-31-95-144.ec2.internal>
2020-05-11 18:18:51 +0000

    ...

--- nic.tf
+++ nic.tf
@@ -6,7 +6,7 @@ resource "azurerm_network_interface" "nic1" {
     name = "testconfiguration1" 
     subnet_id = "${azurerm_subnet.private1.id}" 
     private_ip_address_allocation = "Dynamic" 
-    public_ip_address_id = "${azurerm_public_ip.IP1.id}" 
+    public_ip_address_id = "${azurerm_public_ip.IP.id}" 
   } 
 } 
  
