9cc55b7ad8c4bb761cfa1fcac68db40507348a20
EC2 Default User <ec2-user@ip-172-31-95-144.ec2.internal>
2020-05-11 14:22:32 +0000

    vnet commented

--- 4-vnet.tf
+++ 4-vnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_resource_group" "vnet1" {
-  name     = "${var.vnet_net}"
-  location = "${var.location}"
-  resource_group_name ="${var.resource_group_name}"
-  address_space = ["${var.address_space}"]
-}
+// resource "azurerm_resource_group" "vnet1" {
+//   name     = "${var.vnet_net}"
+//   location = "${var.location}"
+//   resource_group_name ="${var.resource_group_name}"
+//   address_space = ["${var.address_space}"]
+// }
